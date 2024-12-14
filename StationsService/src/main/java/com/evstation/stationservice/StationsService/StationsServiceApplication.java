27f38fca9a4c3bda28f6553af54c4f643bcc6d27
harshaldesale1418 <130337590+harshaldesale1418@users.noreply.github.com>
2024-12-14 14:43:01 +0530

    Add files via upload

---
+++ StationsService/src/main/java/com/evstation/stationservice/StationsService/StationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evstation.stationservice.StationsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationsServiceApplication.class, args);
+	}
+
+}
---
+++ StationsService/src/test/java/com/evstation/stationservice/StationsService/StationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evstation.stationservice.StationsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
